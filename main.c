218f58fa6fb30252158a6b6c6eee8e7bf58df509
Forrest <forrest.c.koch@gmail.com>
2016-03-23 13:57:50 +0000

    added main

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define FILENAME_LENGTH 20
+
+int main(int argc, char *argv[]){
+
+    char inputFile[FILENAME_LENGTH];
+    char outputFile[FILENAME_LENGTH];
+
+    // Should we move this out of main?
+    if(argc != 3){
+	printf("Proper usage is \"./Prog input.file output.file\".\n");
+	exit(1);
+    }
+
+    // Get file names
+    strcpy(inputFile, argv[1]);
+    strcpy(outputFile, argv[2]);
+
+
+
+    return EXIT_SUCCESS;
+}
