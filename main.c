1752a65f11ea5fe43f7c649c503722a857a92dfc
Forrest <forrest.c.koch@gmail.com>
2016-06-07 23:14:58 +0000

    trying to write more clear code?

--- main.c
+++ main.c
@@ -4,18 +4,24 @@
 
 int main(int argc, char *argv[]){
     
-    if(argc != 5){
+    if(argc != 6){
         printf("Proper usage is: ./Run in.file out.file dataMod sortMod threadPower\n");
         exit(1);
     }
 
+    // 2^depth will be the # threads used
     size_t depth = atoi(argv[5]);
+
+    // input/output files
     char *input = argv[1];
     char *output = argv[2];
+
+    // name of data & sorting mods to be used
     char *dataMod = argv[3];
     char *sortMod = argv[4];
 
 
+    //send request
     sortRequestHandler(depth, input, output, dataMod, sortMod);
 
     return 0;
